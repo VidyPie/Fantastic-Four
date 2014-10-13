c40e8abf2f336cac0b9aef993482a900d9324aff
VidyPie <vidar.nedregard@yahoo.no>
2014-10-13 14:59:27 +0200

    forslag interface
    ready to be php'ed

--- MultikopterBygger/steg2.php
+++ MultikopterBygger/steg2.php
@@ -18,13 +18,15 @@ Fantastic Four
         </div>
 
         <div class="frame2">
-            //<div class="inframe_text">
-            <div class="left_box">dsgadsgdsfgdefgrewgrhg
+            <div class="left_box"><b>Forslag 1</b><br><br>motor<br>propell<br>kontrollbrett<br>esc<br>batteri<br>
+            <br><br><br>
+            <a href="http://vg.no"><img class="left_velg" src="main_styling/velg.png"></a>
+            <a href="http://db.no"><img class="left_config" src="main_styling/config.png"></a>
             </div>
-            <div class="right_box">rgyerhgfreuignreg
-            </div>  
-           
-            //</div>
+            <div class="right_box"><b>Forslag 2</b><br><br>motor<br>propell<br>kontrollbrett<br>esc<br>batteri<br>
+            <br><br><br>
+            <a href="http://smp.no"><img class="right_velg" src="main_styling/velg.png"></a>
+            <a href="http://reddit.com"><img class="right_config" src="main_styling/config.png"></a>
         </div>
     </body>
-</html>
+</html>                 
