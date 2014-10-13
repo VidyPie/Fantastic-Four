19c2102adf838e5c1e02a4394df79d3d08ffce51
noenik <niklas.noem@gmail.com>
2014-10-13 15:22:55 +0200

    skikkelig php

--- MultikopterBygger/resultat.php
+++ MultikopterBygger/resultat.php
@@ -23,14 +23,18 @@ $videoopptak = $_POST['videoopptak'];
 $airtime = $_POST['airtime'];
 $gps = $_POST['gps'];
 
-$query = "SELECT SpesifikasjonID";
-$query .= "FROM Spesifikasjoner";
-$query .= "WHERE Videoopptak=" . $videoopptak 
-        . " Rekkevidde=" . $airtime . " GPS=" . $gps . ";";
+$query = "SELECT SpesifikasjonID ";
+$query .= "FROM spesifikasjoner ";
+$query .= "WHERE Videoopptak='" . $videoopptak 
+        . "' and Rekkevidde='" . $airtime . "' and GPS='" . $gps . "';";
 
 $result = mysqli_query($con, $query);
 
-$query2 = "SELECT * FROM oppskrift WHERE OppskriftID = " . 1;
+while($row = mysqli_fetch_array($result)) {
+    $queryResult = $row['SpesifikasjonID'];
+}
+
+$query2 = "SELECT Beskrivelse FROM oppskrift WHERE OppskriftID = " . $queryResult;
 
 $result2 = mysqli_query($con, $query2);
 
