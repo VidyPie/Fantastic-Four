f2f46b7c9a13cdc03014a167715420682eaeede5
Øystein Rindal <oystein_rindal@hotmail.com>
2014-10-15 14:16:40 +0200

    updatezzz

--- MultikopterBygger/steg2.php
+++ MultikopterBygger/steg2.php
@@ -42,35 +42,46 @@ Fantastic Four
         <div class="frame2">
         
             <div class="left_box"><b>Forslag 1</b>
-                <br>
+                <br><br>
            <?php
                 $con = $_SESSION['connection'];
-            
-            
             $query = "SELECT * FROM oppskrift";
             $result = mysqli_query($con, $query);
             while ($row = mysqli_fetch_array($result)) {
                 $beskrivelseResult = $row['Beskrivelse'];
                 echo $beskrivelseResult;
             }
-                ?>
+             ?>
                  <br><br>motor<br>
              
                 propell<br>
                 kontrollbrett<br>
                 esc<br>
                 batteri<br>
-            <br><br><br>
-               
-        
+                <br><br><br>
+
             <a href="http://vg.no"><img class="left_velg" src="main_styling/velg.png"></a>
             <a href="http://db.no"><img class="left_config" src="main_styling/config.png"></a>
             </div>
-            
-            
-            
-            <div class="right_box"><b>Forslag 2</b><br><br>motor<br>propell<br>kontrollbrett<br>esc<br>batteri<br>
             <br><br><br>
+            <div class="right_box"><b>Forslag 2</b>
+                <br><br>
+                 <?php
+                $con = $_SESSION['connection'];
+            $query = "SELECT * FROM oppskrift";
+            $result = mysqli_query($con, $query);
+            while ($row = mysqli_fetch_array($result)) {
+                $beskrivelseResult = $row['Beskrivelse'];
+                echo $beskrivelseResult;
+            }
+             ?>
+                <br>
+                <br>motor
+                <br>propell
+                <br>kontrollbrett
+                <br>esc
+                <br>batteri<br>
+                <br><br><br>
             <a href="http://smp.no"><img class="right_velg" src="main_styling/velg.png"></a>
             <a href="http://reddit.com"><img class="right_config" src="main_styling/config.png"></a>
         </div>
