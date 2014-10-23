7d992ed37e0bf3e8c6fffdfc537d39a44834cdbe
niknoe <niklas.noem@gmail.com>
2014-10-23 17:50:17 +0200

    Glømte å legge til skriptet da...

---
+++ MultikopterBygger/dbConnect.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+$host = "127.0.0.1";
+$port = 3306;
+$socket = "/tmp/mysql.sock";
+$user = "user";
+$password = "123";
+$dbname = "kopterbygger";
+
+$con=  mysqli_connect($host, $user, $password, $dbname, $port, $socket);
+
+$_SESSION['connection'] = $con;
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect ot MySQL: " . mysqli_connect_errno();
+}
