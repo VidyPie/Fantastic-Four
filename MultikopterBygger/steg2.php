41f1d0704091874814d40be9f8a6a283926e0352
noenik <niklas.noem@gmail.com>
2014-10-23 10:31:41 +0200

    Joda, no funka det

--- MultikopterBygger/steg2.php
+++ MultikopterBygger/steg2.php
@@ -55,22 +55,22 @@ Fantastic Four
                 echo ' <div id="left_box"><b>Forslag 1</b> <br><br>';
 
                 $query = 'SELECT o.Beskrivelse, m.Navn AS motor, b.*, esc.Navn AS esc, kon.Navn AS kbrett, p.*  '
-                        . 'FROM spesifikasjoner AS s, oppskrift AS o, komponenter AS kom, motor AS m, batteri AS b, esc, kontrollbrett AS kon, propeller AS p'
+                        . 'FROM spesifikasjoner AS s, oppskrift AS o, komponenter AS kom, motor AS m, batteri AS b, esc, kontrollbrett AS kon, propeller AS p '
                         . 'WHERE s.SpesifikasjonID = o.SpesifikasjonID AND o.KomponenterID = kom.KomponenterID '
                         . 'AND kom.MotorID = m.MotorID AND kom.BatteriID = b.BatteriID AND kom.ESCID = esc.ESCID '
                         . 'AND kom.KontrollbrettID = kon.KontrollbrettID AND kom.PropellID = p.PropellID AND s.SpesifikasjonID = ' . $specID;
                 $result = mysqli_query($con, $query);
                 
                 while ($row = mysqli_fetch_array($result)) {
+                    echo "<b>";
+                    echo $row['Beskrivelse'];
+                    echo "</b><br>";
                     echo $row ['motor'];
                     echo "<br>";
                     echo $row ['esc'];
                     echo "<br>";
                     echo $row ['kbrett'];
                     echo "<br>";
-                    echo "<b>";
-                    echo $row['Beskrivelse'];
-                    echo "</b><br>";
                     echo $row ['Prop_dia'] . '"x' . $row ['Prop_vin'];
                     echo "<br>";
                     echo $row ['Celler'] . 'S ' . $row ['mah'] . 'mah ' . $row ['C_max'] . 'C';
