3720eda5261ee98f523e240b9da645c969c55ba0
niknoe <niklas.noem@gmail.com>
2014-10-23 17:49:17 +0200

    Flytta oppkoblinga mot DB i eget skript og fjerna maaaaasse duplisert kode

--- MultikopterBygger/pktConfig.php
+++ MultikopterBygger/pktConfig.php
@@ -1,20 +1,6 @@
 <?php
 include 'checklogin.php';
-
-$host = "127.0.0.1";
-$port = 3306;
-$socket = "/tmp/mysql.sock";
-$user = "user";
-$password = "123";
-$dbname = "kopterbygger";
-
-$con = mysqli_connect($host, $user, $password, $dbname, $port, $socket);
-
-$_SESSION['connection'] = $con;
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect ot MySQL: " . mysqli_connect_errno();
-}
+include 'dbConnect.php';
 ?>
 <!DOCTYPE html>
 <!--
--- MultikopterBygger/steg2.php
+++ MultikopterBygger/steg2.php
@@ -1,20 +1,5 @@
 <?php
-session_start();
-
-$host = "localhost";
-$port = 3306;
-$socket = "/tmp/mysql.sock";
-$user = "user";
-$password = "123";
-$dbname = "kopterbygger";
-
-$con = mysqli_connect($host, $user, $password, $dbname, $port, $socket);
-
-$_SESSION['connection'] = $con;
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect ot MySQL: " . mysqli_connect_errno();
-}
+include 'dbConnect.php';
 ?>
 
 <!DOCTYPE html>
