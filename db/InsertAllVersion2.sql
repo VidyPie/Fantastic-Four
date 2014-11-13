909af06fd8f16999fc894448a85965265bfd3bed
noenik <niklas.noem@gmail.com>
2014-11-13 11:51:49 +0100

    fiksa litt css

--- db/InsertAllVersion2.sql
+++ db/InsertAllVersion2.sql
@@ -208,10 +208,10 @@ DELIMITER ;;
 CREATE PROCEDURE compID(param1 int)
 BEGIN
 SELECT m.MotorID, b.BatteriID, esc.ESCID, kon.KontrollbrettID, p.PropellID
-FROM spesifikasjoner AS s, oppskrift AS o, komponenter AS kom, motor AS m, batteri AS b, esc, kontrollbrett AS kon, propeller AS p
-WHERE s.SpesifikasjonID = o.SpesifikasjonID AND o.KomponenterID = kom.KomponenterID
-AND kom.MotorID = m.MotorID AND kom.BatteriID = b.BatteriID AND kom.ESCID = esc.ESCID
-AND kom.KontrollbrettID = kon.KontrollbrettID AND kom.PropellID = p.PropellID AND s.SpesifikasjonID = param1;
+FROM oppskrift AS o, komponenter AS kom, motor AS m, batteri AS b, esc, kontrollbrett AS kon, propeller AS p
+WHERE o.KomponenterID = kom.KomponenterID AND kom.MotorID = m.MotorID 
+AND kom.BatteriID = b.BatteriID AND kom.ESCID = esc.ESCID
+AND kom.KontrollbrettID = kon.KontrollbrettID AND kom.PropellID = p.PropellID AND o.OppskriftID = param1;
 END;;
 
 DELIMITER ;;
